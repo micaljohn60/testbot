4e85ceef36b7fee1a94a84e0856ca514eb26f2c6
micaljohn60 <micaljohn60@gmail.com>
2021-02-05 17:48:48 +0630

    ggg

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.listen((process.env.PORT || 3000));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 // Server frontpage
 app.get('/', function (req, res) {
